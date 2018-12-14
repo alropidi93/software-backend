c8c21700eb5d6a55947a5ae73bb04f0f26ce2074
CarlosRebazaC <rebaza.carlose@pucp.pe>
2018-12-14 01:16:29 -0500

    setProductos en tabla, dada categoria

--- app/Repositories/CategoriaRepository.php
+++ app/Repositories/CategoriaRepository.php
@@ -5,12 +5,13 @@ use App\Models\Categoria;
 	
 class CategoriaRepository extends BaseRepository{
     
+    protected $productoRepository;
 
    
-    public function __construct(Categoria $categoria)
+    public function __construct(Categoria $categoria,ProductoRepository $productoRepository)
     {
         $this->model = $categoria;
-        
+        $this->productoRepository=$productoRepository;
     }
 
    
@@ -20,6 +21,28 @@ class CategoriaRepository extends BaseRepository{
         return $this->model = $this->model->create($dataArray);
         
     }
+
+    public function setProductosDeCategoriaEnProductoXDescuento($idCategoria, $descuento, $descuentoData){
+        $productos = DB::table('producto')->where('deleted', false)->get();
+        $listaProductosCategoria = array();
+        foreach($productos as $key => $producto){
+            if($idCategoria == $producto->idCategoria){
+                $listaProductosCategoria[] = $producto;
+            }
+        }
+        foreach($listaProductosCategoria as $key => $producto){
+            $this->productoRepository->setModel($producto);
+            $this->productoRepository->attachProductoXDescuento($descuento, $producto->id, $descuentoData['idTienda']);
+        }
+
+
+
+       
+    }
     
+    public function attachCategoriaXDescuento($descuento, $idCategoria, $idTienda){
+        $this->model->descuentosTc()->save($descuento , ['idTienda'=>$idTienda, 'idCategoria'=> $idCategoria, 'deleted'=>false] );
+        $this->model->save();
+    }
     
 }
